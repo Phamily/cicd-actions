76c23bbdc22df51db22cb29c76345bbd86692dd9
Alan Graham <alangraham5@gmail.com>
2020-03-04 04:40:42 +0000

    Initial commit

---
+++ build_test_push/main.rb
@@ -0,0 +1,67 @@
+#! /usr/bin/env ruby
+
+USERNAME = ENV['INPUT_USERNAME']
+PASSWORD = ENV['INPUT_PASSWORD']
+REGISTRY = ENV['INPUT_REGISTRY']
+NAME = ENV['INPUT_NAME']
+DEBUG_MODE = ENV['INPUT_DEBUG_MODE'] == "true"
+TESTS = (ENV['INPUT_TESTS'] || []).split(",")
+TEST_ENV_FILE = ENV['TEST_ENV_FILE']
+
+BASENAME = NAME.split("/")[-1]
+
+def run
+  login unless DEBUG_MODE
+  build
+  test
+  push unless DEBUG_MODE
+end
+
+def login
+  sh "echo #{PASSWORD} | docker login -u #{USERNAME} --password-stdin #{REGISTRY}"
+end
+
+def build
+  sh "docker build . -t #{BASENAME}"
+end
+
+def test
+  env_file_opt = ""
+  if TEST_ENV_FILE != nil
+    env_file_opt= "--env-file=#{TEST_ENV_FILE}"
+  end
+  TESTS.each do |test|
+    puts "Running test: #{test}"
+    sh "docker run #{env_file_opt} #{BASENAME} #{test}"
+  end
+end
+
+def push
+  branch = branch
+  if branch.nil?
+    puts "Only pushing images for branches."
+    return
+  end
+  tag = branch
+  full_remote_image = "#{REGISTRY}/#{NAME}:#{tag}"
+  sh "docker tag #{BASENAME}:latest #{full_remote_image}"
+  sh "docker push #{full_remote_image}"
+end
+
+# helper methods
+
+def branch
+  if GITHUB_REF.include?("refs/heads")
+    return GITHUB_REF.split("/")[-1]
+  else
+    return nil
+  end
+end
+
+def sh(cmd)
+  puts "Running: #{cmd}"
+  ret = system(cmd)
+  raise "Command did not finish successfully" if ret != true
+end
+
+run
---
+++ build_test_push/test.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Building..."
+sudo docker build . -t cicd-actions:latest
+
+echo "Testing..."
+sudo docker run -e INPUT_DEBUG_MODE=true -e INPUT_NAME=cicd-test -e INPUT_TESTS=run_test.sh -v `pwd`/test_repo:/test_repo -w /test_repo cicd-actions:latest
