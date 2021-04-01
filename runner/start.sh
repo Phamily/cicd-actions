c5b9107c6eb3b560ff2ab18865698f759d58738a
Alan Graham <alangraham5@gmail.com>
2021-04-01 22:10:28 +0000

    Support for foreman

--- runner/start.sh
+++ runner/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+INSTNAME=${1:-$HOSTNAME}
 REPO=$PHAMILY_REPO
 ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN
 
 export RUNNER_ALLOW_RUNASROOT="1"
 
-cp -a /root/actions-runner/base /root/actions-runner/$HOSTNAME
-cd /root/actions-runner/$HOSTNAME
+cp -a /root/actions-runner/base /root/actions-runner/$INSTNAME
+cd /root/actions-runner/$INSTNAME
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
@@ -18,7 +19,7 @@ cleanup() {
     REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
     ./config.sh remove --unattended --token ${REG_TOKEN} || true
     echo "Cleaning up runner directory..."
-    rm -rf /root/actions-runner/$HOSTNAME
+    rm -rf /root/actions-runner/$INSTNAME
 }
 
 trap 'cleanup; exit 130' INT
